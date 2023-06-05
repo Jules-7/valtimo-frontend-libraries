f7cd6c84004a690630f26487f70dcf1f304f86ce
Teodor Dragoi <teodor.dragoi@ritense.com>
2023-06-05 12:57:33 +0200

    feat: Add boilerplate for Dashboard management

---
+++ projects/valtimo/dashboard-management/src/lib/dashboard-management/dashboard-management.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './dashboard-management.component.html',
+  styleUrls: ['./dashboard-management.component.scss']
+})
+export class DashboardManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
